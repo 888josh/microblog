8b4753c637b090ac88e3c848e1dd37be382d8acd
Joshua Nghe <121520048+888josh@users.noreply.github.com>
2024-01-11 19:01:12 -0700

    Add files via upload

---
+++ microblog.py
@@ -0,0 +1,9 @@
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+##run flask shell to create python interpreter in context of the app.
+
